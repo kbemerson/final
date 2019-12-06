9cc4cd8ce6fe3ecf7a7f6e090b79a6a6ef7cfb41
Katie Emerson <kbemerson27@gmail.com>
2019-12-06 10:15:29 -0700

    Day 2. Finished responses and started js code.

---
+++ final.js
@@ -0,0 +1,11 @@
+function ready () {
+    console.log("Page Ready");
+
+    let feeling = document.forms.feeling;
+    let better = document.forms.better;
+    let symptoms = document.forms.symptoms;
+
+
+
+}
+document.addEventListener("DOMContentLoaded", ready);
